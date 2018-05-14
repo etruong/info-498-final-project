01ad5b2088aee2ebcae26774c73abd420a71f018
Elisa <elisat15@uw.edu>
2018-05-14 09:53:31 -0700

    set up

---
+++ ui.R
@@ -0,0 +1,34 @@
+library (shiny)
+
+my.ui <- fluidPage (
+  
+  navbarPage ("UW Resilience Signs",
+              
+              tabPanel ("Home"
+                # About the Signs
+                # About
+              ),
+              
+              tabPanel ("Methods"
+                # Sample Demographics
+                # Survey Development
+                # Limitations
+              ),
+              
+              tabPanel ("Experience Microaggression/Microcompassion"
+                        
+              ),
+              
+              tabPanel ("Perception of Signs"
+                # Hayden WordCloud
+              ),
+              
+              tabPanel (""
+                        
+              )
+              
+  )
+  
+)
+
+# Word Cloud
