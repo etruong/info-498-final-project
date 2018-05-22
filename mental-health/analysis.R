c9894ac32b1418e41631dbd025d85bc0efaf4d1d
Kassandra Franco <kassandrafranco@gmail.com>
2018-05-21 20:34:07 -0700

    Temporarily added mental-health folder

---
+++ mental-health/analysis.R
@@ -0,0 +1,34 @@
+
+# setwd("~/Desktop/Classes/INFO_498/wb-8-kasfranco/MentalHealthAndMicroExperiences")
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(reshape2)
+library(plotly)
+
+data <- read.csv("./ResilienceLabSignSurvey.csv")
+prepped_data <- 
+  select(data, 
+         How.would.you.rate.your.mental.health.., 
+         In.relation.to.your.peers..to.what.degree.do.you.experience.microaggressions., 
+         In.relation.to.your.peers..to.what.degree.do.you.experience.microcompassions.) #,
+
+colnames(prepped_data) <- 
+  c("ment.health", "microaggression", "microcompassion") #, "effective") 
+  # ment.health: 1 poor, 5 excellent
+  # microaggression: 1 less, 5 more
+  # microcompassion: 1 less, 5 more
+
+prepped_data <- 
+  filter(prepped_data, !is.na(ment.health)) %>% 
+  filter(!is.na(microaggression)) %>% 
+  filter(!is.na(microcompassion))
+
+gathered_data <- gather(prepped_data, "microexperience", "rating", 2:3) 
+
+plot <- 
+  ggplot(gathered_data, aes(x = ment.health, y = rating, col = microexperience)) +
+  geom_jitter()
+
+plotly <- ggplotly(plot)
---
+++ mental-health/server.R
@@ -0,0 +1,50 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+# setwd("~/Desktop/Classes/INFO_498/wb-8-kasfranco/MentalHealthAndMicroExperiences")
+library(shiny)
+library (plotly)
+source("analysis.R")
+
+shinyServer(function(input, output) {
+  
+  # filtered_data <- reactive ({
+  #   if(input$experience != "both") {
+  #     gathered_data <- filter(gathered_data, microexperience == input$experience)  
+  #   } 
+  #   gathered_data <- filter(gathered_data, ment.health == input$rating.health)
+  #   return(gathered_data)
+  # })
+  
+  filtered_data <- reactive ({
+    validate (
+      need (input$rating.health != "", "Please select data")
+    )
+    if(input$experience != "both" & 
+       (input$rating.health == 1 | input$rating.health == 2 |
+        input$rating.health == 3 | input$rating.health == 4 |
+        input$rating.health == 5)) {
+      gathered_data <- filter(gathered_data, microexperience == input$experience) %>% 
+                       filter(rating == input$rating.health)
+    }
+    gathered_data <- filter(gathered_data, ment.health == input$rating.health)
+    return(gathered_data)
+  })
+  
+  output$plotly <- renderPlot({
+    plot <- ggplot (filtered_data(), aes(x = ment.health, y = rating, col = microexperience)) +
+      geom_jitter() +  #height = 0.3, width = 0.3
+      labs(x = "Rating of Own Mental Health (Poor(1) to Excellent(5))",
+           y = "Perception of Microexperience (Relatively Less(1) to Relatively More(5))",
+           col = "Microexperience, Relative to Peers") #+ 
+           #xlim(0.5, 5.5) + ylim(0.5, 5.5)
+    return(plot)
+  })
+  
+})
